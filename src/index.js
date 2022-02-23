a80127a3110437603300b0159a1fe48e341eded4
Pavel Hohlov <pashahohlov54@gmail.com>
2022-02-24 00:45:22 +0300

    Калькулятор не готов

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import modal from './modules/modal'
 import timer from './modules/timer'
 import servicesModal from './modules/services-modal'
-import form from './modules/form'
 import scroll from './modules/scroll'
 import calculator from './modules/caculator'
 
 modal()
 timer()
 servicesModal()
-form()
 scroll()
 calculator()
--- src/modules/caculator.js
+++ src/modules/caculator.js
@@ -1,5 +1,30 @@
-const calculator = () => {
+const calculator = (price = 1000) => {
+    const calcBlock = document.querySelector('#calc')
+    const calcType = document.querySelector('#calc-type')
+    const calcTypeMaterial = document.querySelector('#calc-type-material')
+    const calcSquare = document.querySelector('#calc-input')
+    const calcTotal = document.querySelector('#calc-total')
 
+    const countCalc = () => {
+        const calcTypeValue = +calcType.options[calcType.selectedIndex].value
+        const calcTypeMaterialValue = +calcTypeMaterial.options[calcTypeMaterial.selectedIndex].value
+        const calcSquareValue = calcSquare.value
+        let calcTotalValue = 0
+
+        if (calcType.value && calcTypeMaterial.value && calcSquare.value) {
+            calcTotalValue = price * calcTypeValue * calcTypeMaterialValue * calcSquareValue
+        } else {
+            calcTotalValue = 0
+        }
+
+        calcTotal.textContent = calcTotalValue
+    }
+
+    calcBlock.addEventListener('input', (e) => {
+        if (e.target === calcType || e.target === calcTypeMaterial || e.target === calcSquare) {
+            countCalc()
+        }
+    })
 }
 
 export default calculator
