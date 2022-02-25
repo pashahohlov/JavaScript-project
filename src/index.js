7e7716ac9c086dd8cc398974940903ed999b14d6
Pavel Hohlov <pashahohlov54@gmail.com>
2022-02-25 13:39:52 +0300

    Подключена форма

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import timer from './modules/timer'
 import servicesModal from './modules/services-modal'
 import scroll from './modules/scroll'
 import calculator from './modules/caculator'
+import form from './modules/form'
 
 modal()
 timer()
 servicesModal()
 scroll()
 calculator()
+form()
